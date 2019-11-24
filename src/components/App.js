9fc3add68505716a40e115eee1e095699f48364c
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-11-24 10:26:07 -0300

    310. Rendering the Language Store

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
-import LanguageSelecor from './LanguageSelector';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-
-    state = { language: 'english' };
-
-    onLanguageChange = language => {
-        this.setState({ language });
-    }
-
     render() {
         return (
             <div className="ui container">
-
-                <LanguageSelecor onLanguageChange={this.onLanguageChange} />
-
-                <ColorContext.Provider value='red'>
-                    <LanguageContext.Provider value={this.state.language}>
-                        <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="red">
+                            <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
