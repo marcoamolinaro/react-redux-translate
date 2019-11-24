2357ac6ee6ab50007564aaa3722a1e66fdda31ce
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-11-24 10:37:42 -0300

    312. Connecting the Field and Button to the Store

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,15 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-    renderSubmit(value) {
-        return value === 'english' ? 'Submit' : 'Vorleggen';
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Vorleggen';
     }
 
     renderButton(color) {
         return (
             <button className={`ui button ${color}`}>
                 <LanguageContext.Consumer>
-                    {(value) => this.renderSubmit(value)}
+                    {({language}) => this.renderSubmit(language)}
                 </LanguageContext.Consumer>
             </button>         
         );
